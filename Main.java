3038ca89ea7c94b8b560b8f74432b278718426cb
yutorikohei <yutoriseikatu49@gmail.com>
2021-07-04 12:42:52 +0900

    Main.javaを修正

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster!");
+    System.out.println("SEMONSTER!!");
     Monster monster = new Monster();
     System.out.println(monster.toString());
   }
